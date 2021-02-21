432d2520beb373de1ce2bf95fe8bb4cc72ac8d64
mahipalsa <mahipalchoudhary@hotmail.com>
2021-02-21 23:17:58 +0530

    commiting

---
+++ ui/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { MainPageComponent } from './main-page/main-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    MainPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ui/src/app/top-bar/top-bar.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TopBarRoutingModule } from './top-bar-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TopBarRoutingModule
+  ]
+})
+export class TopBarModule { }
